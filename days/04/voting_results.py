c41b6647cf321bda8f014a0c01c001e6ef8ed84b
shaversj <shaversj@gmail.com>
2018-09-22 07:43:33 -0400

    Update

--- days/04/voting_results.py
+++ days/04/voting_results.py
@@ -17,20 +17,11 @@ def read_data():
     """
     base_folder = os.path.dirname(__file__)
     filename1 = os.path.join(base_folder, 'data', 'election_data_1.csv')
-    filename2 = os.path.join(base_folder, 'data', 'election_data_2.csv')
 
-    # Read first file
     with open(filename1, 'r', encoding='utf-8') as f1:
         reader1 = csv.DictReader(f1)
 
         for line in reader1:
-            votes.append(line.get('Candidate'))
-
-    # Read second file
-    with open(filename2, 'r', encoding='utf-8') as f2:
-        reader2 = csv.DictReader(f2)
-
-        for line in reader2:
             votes.append(line.get('Candidate'))
 
 
@@ -42,10 +33,16 @@ def calculate_winner():
 def count_and_print_results():
     """Count the number of votes for each candidate.
 
-    Total Votes:     4324001
-    -------------------------
-    Khan:     51%    2218231
-    Correy:   16%    704200
+    >>>>
+    Total Votes:      803000
+    ---------------------------------------
+    Vestal:   48%     385440
+    Torres:   44%     353320
+    Seth:     5%      40150
+    Cordin:   3%      24090
+    ---------------------------------------
+    Winner: Vestal
+    ---------------------------------------
 
     """
 
@@ -83,24 +80,16 @@ def count_and_print_results():
     print('---------------------------------------')
     print('-----------Election Results------------')
     print('---------------------------------------')
-    print(f'Total Votes:     {total}')
+    print(f'Total Votes:      {total}')
     print('---------------------------------------')
-    print(
-        f'Khan:     {round(int(count_khan) / int(total) * 100.0)}%    {(count_khan)}')
-    print(
-        f'Correy:   {round(int(count_correy) / int(total) * 100.0)}%    {(count_correy)}')
-    print(
-        f'Li:       {round(int(count_li) / int(total) * 100.0)}%    {(count_li)}')
     print(
         f'Vestal:   {round(int(count_vestal) / int(total) * 100.0)}%     {(count_vestal)}')
     print(
         f'Torres:   {round(int(count_torres) / int(total) * 100.0)}%     {(count_torres)}')
     print(
-        f'OTooley:  {round(int(count_otooley) / int(total) * 100.0)}%     {(count_otooley)}')
+        f'Seth:     {round(int(count_seth) / int(total) * 100.0)}%      {(count_seth)}')
     print(
-        f'Seth:     {round(int(count_seth) / int(total) * 100.0)}%     {(count_seth)}')
-    print(
-        f'Cordin:   {round(int(count_cordin) / int(total) * 100.0)}%     {(count_cordin)}')
+        f'Cordin:   {round(int(count_cordin) / int(total) * 100.0)}%      {(count_cordin)}')
     print('---------------------------------------')
     print(f'Winner: {calculate_winner()}')
     print('---------------------------------------')
