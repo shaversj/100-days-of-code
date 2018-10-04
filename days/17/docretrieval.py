22576006d0db31ccb846dddae2bf0b9537b41293
shaversj <shaversj@gmail.com>
2018-10-04 18:35:47 -0400

    Day 17 Updates

---
+++ days/17/docretrieval.py
@@ -0,0 +1,32 @@
+word_to_docs = {}
+documents = {}
+
+
+def main():
+    pass
+
+
+def read_file():
+    with open('ap_docs2.txt', 'r', encoding='utf-8') as f:
+
+    pass
+
+
+def parse_file() -> dict:
+    """
+    Your program will read in the documents from the file and number each document starting with 1 (the first document in the file is document 1, the second is document 2, etc.). In order to look up search terms, we will need to know which words appear in each document. We will use a dictionary for this purpose. Each entry in your dictionary should have a word as the key and the word’s value as the set of documents that this word appears in. This arrangement allows you to look up a keyword in the dictionary and immediately get all the documents that it appears in, making it easy to figure out documents that might meet a search query.
+
+    """
+
+    pass
+
+
+def search_documents():
+    """
+    Search for documents that match the search words input by the user.
+    """
+    pass
+
+
+def display_document():
+    pass
