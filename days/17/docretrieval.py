82421db161c8711c9d6d0c7580ed2d778315858b
shaversj <shaversj@gmail.com>
2018-10-05 16:04:20 -0400

    Day 17 Updates

--- days/17/docretrieval.py
+++ days/17/docretrieval.py
@@ -1,16 +1,57 @@
+import string
+
 dictOfDocs = {}
-documents = {}
+words = {}
+dictOfWords = {}
 
 
 def main():
     pass
 
 
-def parse_file():
+def read_file() -> list:
 
+    # Read the entire txt file as one string
     data = open('ap_docs2.txt').read()
+
+    # Split the string at '<NEW DOCUMENT>'
     docs = data.split('<NEW DOCUMENT>')
-    dictOfDocs = {k: docs[k] for k in range(0, len(docs))}
+
+    return docs, data
+
+
+def strip_chars_from_str(data: str):
+    """ Clean the data so that you can split the string into individual words. Find a more efficient way to clean the data."""
+    data_lower = data.lower()
+    data_lower = data_lower.replace('<new document>', "")
+    data_lower = data_lower.replace('\n', "")
+    data_lower = data_lower.replace('string.punctuation', "")
+    data_lower = data_lower.replace('.', "")
+    data_lower = data_lower.replace(',', "")
+
+
+def add_numbers_to_docs(docs: list):
+
+    # Add document numbers to seperated list.
+    for k in range(0, len(docs)):
+        dictOfDocs[k] = docs[k]
+
+
+def build_search_set(words: list):
+    """
+    Create a dictionary that stores single words as the key, and value as the set of documents (numbers) that the word appears in.
+
+    >>> dictOfWords
+    {'on': {1, 2, 3} 'the': 1, 2, 3 'bright': 1, 'side': 1, 2 'of': 1, 2 'look': 1, 2 'very': 2, 'there': 3, 'moon': 2, 3}
+    """
+
+    for k, v in dictOfDocs.items():
+        for word in words:
+            if word not in v:
+                dictOfWords[word] = k
+            else:
+                dictOfWords[word] += k
+                break
 
 
 def search_documents():
--- log.md
+++ log.md
@@ -135,3 +135,10 @@ I also added a function that finds the max value in a dictionary. This was a lit
 **Today's Focus**: Began working on a document retrieval project from [MSU Python Projet Archive](http://www.cse.msu.edu/~cse231/PracticeOfComputingUsingPython/06_Dictionaries/DocRetrieval/)
 
 **Link to work**: [Github](days/17)
+
+----
+### Day 18: October 5, 2018
+
+**Today's Focus**: Continued to work on a document retrieval project from [MSU Python Projet Archive](http://www.cse.msu.edu/~cse231/PracticeOfComputingUsingPython/06_Dictionaries/DocRetrieval/). Started to learn about how to add sets as values in a dictionary.
+
+**Link to work**: [Github](days/17)
