cc6bff85e7fd2fb70a1b4149d8865cf821afa2eb
shaversj <shaversj@gmail.com>
2018-10-10 08:58:20 -0400

    Created the distance method

--- days/21/__init__.py
+++ days/21/__init__.py
@@ -1,4 +1,5 @@
 import config
+import requests
 
 
 class Tour:
@@ -28,8 +29,28 @@ class Tour:
 
         return '{self.__class__.__name__}({self.origin}, {self.destination})'.format(self=self)
 
-    def distance(self):
-        pass
+    def distance(self, *mode: str):
+        """
+        This method takes a single (optional) argument indicating a mode – one of the strings ‘driving’ (default), ‘bicycling’, or ‘walking’. It returns the total distance (in meters) covered by the tour for the indicated mode. This method is where you will use urllib functions to get data from the web to find the distances between consecutive locations in the tour and calculate the total distance. If a response does not contain a distance value, the method should raise a ValueError exception.
+
+        """
+        formatted_destination = ("|".join(self.destination))
+        #mode = mode
+
+        api_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' + \
+            self.origin + '&destinations=' + formatted_destination + \
+            '&' + 'mode=' + str(mode) + '&' + 'key=' + config.api_key
+
+        r = requests.get(api_url)
+
+        response = r.json()
+
+        tour_mileage = response["rows"][0]["elements"][1]["distance"]["text"]
+
+        return tour_mileage
+
+    def destinations(self):
+        return("|".join(self.destination))
 
     def __add__(self):
         pass
@@ -38,5 +59,5 @@ class Tour:
         pass
 
 
-#t1 = Tour('New York, NY', 'Lansing, MI', 'Sacramento, CA')
-#t2 = Tour('New York, NY', 'Sacramento, CA')
+# t1 = Tour('New York, NY', 'Lansing, MI', 'Sacramento, CA')
+# t2 = Tour('New York, NY', 'Sacramento, CA')
--- days/21/main.py
+++ days/21/main.py
@@ -5,6 +5,7 @@ def main():
     t1 = Tour('New York, NY', 'Lansing, MI', 'Sacramento, CA')
     t2 = Tour('New York, NY', 'Sacramento, CA')
 
+    print(t1.destination)
     print(t1.__str__())
     print(t2.__str__())
     print()
@@ -12,6 +13,24 @@ def main():
     print(t2)
     print()
     print(repr(t1))
+    print()
+    # print("|".join(t1.destination))
+    print(Tour.destinations(t1))
+    print(Tour.distance(t1))
+
+    """
+    print("t1: {}\nt2:{}\nt3:{}".format(t1,t2,t3))
+    print("t1 distances: driving-{} km; biking-{} km; walking-{} km".format(
+    round(t1.distance()/1000), round(t1.distance('bicycling')/1000), round(t1.distance('walking')/1000)))
+
+    print("Using driving distances from here on.")
+    t4 = t1 + t2
+    print("t4:", t4)
+    print("t4 driving distance:", round(t4.distance()/1000),"km") print("t4 == t1 + t2:", t4 == t1 + t2)
+
+    """
+    # print(t1)
+    # print("t1 distances: driving-{} km; biking-{} km; walking-{} km".format(round(t1.distance()/1000), round(t1.distance('bicycling')/1000), round(t1.distance('walking')/1000)))
 
 
 if __name__ == '__main__':
